9a8204ea00e9456635f14012bd5374bbbe060646
rxor <175366793+rxor@users.noreply.github.com>
2024-08-01 00:04:50 +0100

    Create gzc.c

---
+++ gzc.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define blud main
+#define skibidi {
+#define gucci }
+#define rn ;
+#define cap 0
+#define no !
+#define bouta &
+#define bussin !=
+#define be ==
+#define sus <
+#define uwu <=
+#define slaps --
+#define ong ++
+#define finna =
+#define yeet +
+#define slay *
+#define yap %
+#define rizz printf
+#define fam int
+#define lit char
+#define bet if
+#define fanum while
+#define ngl for
+#define frfr long
+#define flat return
+#define cook continue
+#define bigyikes break
+#define nah void
+#define oop sizeof
+#define sigma scanf
+#define stan getchar
+#define pookie malloc
+
+fam blud() skibidi
+  frfr frfr fam c rn
+  lit slay p rn
+  fanum(1) skibidi
+    rizz("Enter a number greater than 3: ") rn
+    bet(sigma("%lld", bouta c) bussin 1) skibidi
+      rizz("Big yikes, valid numbers only!\n") rn
+      fanum(stan() bussin '\n') rn
+      cook rn
+    gucci
+    bet(c uwu 3) skibidi
+      rizz("Big yikes, numbers greater than 3 only!\n") rn
+      cook rn
+    gucci
+    p finna pookie(c slay oop(lit)) rn
+    bet(no p) skibidi
+      rizz("Memory allocation didn't pass the vibe check!\n") rn
+      flat 1 rn
+    gucci
+    ngl(fam i finna cap rn i sus c rn ong i) skibidi
+      p[i] finna 'A' yeet (i yap 26) rn
+    gucci
+    fanum(no cap) skibidi
+      bet(no p) skibidi
+        bigyikes rn
+      gucci
+      rizz("%p, %lld\n", (nah slay)p, c) rn
+      p slaps rn 
+    gucci
+    free(p) rn
+    flat cap rn
+  gucci
+gucci
